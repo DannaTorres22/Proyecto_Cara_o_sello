ec420fcd953cc73ea94774ae3223d366697d7bd6
DannaTorres22 <104436270+DannaTorres22@users.noreply.github.com>
2022-11-01 06:27:10 -0500

    js

---
+++ juego.js
@@ -0,0 +1,51 @@
+let cara = 0;
+let sello = 0;
+let nombre;
+let dinero;
+let cont;
+let moneda = document.querySelector(".moneda");
+let girarBtn = document.querySelector("#flip-button");
+let reinicioBtn = document.querySelector("#reinicio-button");
+
+
+girarBtn.addEventListener("click", () => {
+    let i = Math.floor(Math.random() * 2);
+    moneda.style.animation = "none";
+    if(i){
+        setTimeout(function(){
+            moneda.style.animation = "spin-cara 3s forwards";
+        }, 100);
+        cara++;
+    }
+    else{
+        setTimeout(function(){
+            moneda.style.animation = "spin-sello 3s forwards";
+        }, 100);
+        sello++;
+    }
+    setTimeout(updateStats, 3000);
+    disableButton();
+});
+
+
+function updateStats(){
+    document.querySelector("#cara-count").textContent = `cara: ${cara}`;
+    document.querySelector("#sello-count").textContent = `sello: ${sello}`;
+    document.querySelector("#cont-count").textContent = `contador: ${cont}`;
+}
+
+function disableButton(){
+   girarBtn.disabled = true;
+    setTimeout(function(){
+       girarBtn.disabled = false;
+    },3000);
+}
+
+reinicioBtn.addEventListener("click",() => {
+    moneda.style.animation = "none";
+    cara = 0;
+    sello = 0;
+    
+    updateStats();
+});
+ 
