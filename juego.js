df2dc6bbaa89c8442f3b20f02611586617cbbc24
DannaTorres22 <104436270+DannaTorres22@users.noreply.github.com>
2022-11-01 08:47:15 -0500

    cod3

--- juego.js
+++ juego.js
@@ -1,14 +1,11 @@
 let cara = 0;
 let sello = 0;
-const nombre= document.getElementById('ingr');
-let dinero;
+const ing= document.getElementById('ingresar');
 let cont;
 let moneda = document.querySelector(".moneda");
 let girarBtn = document.querySelector("#flip-button");
 let reinicioBtn = document.querySelector("#reinicio-button");
 
-
-Swal.fire(`Bienvenid@" ${nombre}"`);
 
 girarBtn.addEventListener("click", () => {
     let i = Math.floor(Math.random() * 2);
@@ -33,7 +30,6 @@ girarBtn.addEventListener("click", () => {
 function updateStats(){
     document.querySelector("#cara-count").textContent = `cara: ${cara}`;
     document.querySelector("#sello-count").textContent = `sello: ${sello}`;
-    document.querySelector("#cont-count").textContent = `contador: ${cont}`;
 }
 
 function disableButton(){
@@ -43,11 +39,50 @@ function disableButton(){
     },3000);
 }
 
+//reiniciar la partida 
 reinicioBtn.addEventListener("click",() => {
     moneda.style.animation = "none";
     cara = 0;
     sello = 0;
+    cont=0;
+    apuesta=0;
     
     updateStats();
 });
+
+ing.addEventListener('click', ()=>{
+    let opcion = document.getElementById('opcion').value;
+    opcion = parseInt(opcion);
+    let apuesta = document.getElementById('apuest').value;
+    apuesta = parseInt(apuesta);
+
+    let c = 0;
+    let confirmar = true;
+    if ( apuesta >= 1000 ) {
+        do {
+            switch (opcion) {
+                
+                case 1:
+                    swal(`Tu valor apostado es: ${apuesta}`);
+                    
+                    break;
+                case 2:
+                    swal(`Tu valor apostado es: ${apuesta}`)
+                    break;
+            
+                default:
+                    break;
+            }
+             confirmar = window.confirm(`Desea seguir jugando?`)
+        
+        } while (confirmar == true){
+            swal(`Gracias por jugar`)
+        };
+    
+    
+    } else {
+        swal (`Por favor un valor mayor a 1000 `)
+    }
+    
+    });
  
